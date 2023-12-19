72745b6a9ecb6dcf489822cfd7d638fbea23acc7
hssSean <144593163+hssSean@users.noreply.github.com>
2023-12-19 21:49:51 +0800

    Update main3.c

--- hw5/main3.c
+++ hw5/main3.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+#define ninemax 9 
+int main(){
+    int nine_fnt=1;
+    for(int i=1;i<=ninemax;i++){
+    
+        printf("%d*%d=%d\t",nine_fnt,i,nine_fnt*i);
+        if(nine_fnt!=ninemax){
+            if(i==ninemax){
+            i=0;
+            nine_fnt++;
+            printf("\n");
+            }
+        }
+    }
+}
