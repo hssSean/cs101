dd47bf6a95d3695c406dd1000ef5ff781d4b8661
hssSean <144593163+hssSean@users.noreply.github.com>
2023-12-19 21:50:16 +0800

    Update main4.c

--- hw5/main4.c
+++ hw5/main4.c
@@ -1,1 +1,27 @@
-
+#include <stdio.h>
+#define ninemax 9 
+int main(){
+    int i=12345;
+    int prt;
+    if(i>=1000){
+        int a=i-((i/10)*10);
+        int b=((i/1000)*1000-(i/10000)*10000)/1000;
+        prt=i-(1000*b)+(1000*a)-a+b;
+    }
+    else{
+        int num=0;
+        int a=i-((i/10)*10);
+        int count=i;
+        do{
+            num++;
+        }while(count/=10);
+        if(num==1){
+            prt=i*1000;
+        }
+        else{
+            prt=i-a+(1000*a);
+        }
+        
+    }
+    printf("%d",prt);
+}
