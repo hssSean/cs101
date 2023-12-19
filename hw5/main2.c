6dd65a08e2479244b7e937787927a7ae988d9b8f
hssSean <144593163+hssSean@users.noreply.github.com>
2023-12-19 21:49:01 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+#include <math.h>
+int main(){
+int i=1, sign=1;
+double t=1,pi=0;
+while(fabs(t)>=1e-8){
+pi=pi+t;
+sign=-sign;
+i=i+2;
+t=1.0*sign/i;
+}
+printf("%.5f",4*pi);
+}
